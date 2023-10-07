65c64e3915d1e4f2a02732bbd779018fdb1f7a31
AllanPZamora <gelgelacc1@gmail.com>
2023-10-08 03:37:43 +0800

    added reset button

--- Zamora, Allan Gel P. (Forex)/Foreign Exchange Market/Foreign Exchange Market/Form1.vb
+++ Zamora, Allan Gel P. (Forex)/Foreign Exchange Market/Foreign Exchange Market/Form1.vb
@@ -1,7 +1,4 @@
 ﻿Public Class Form1
-
-
-
     '    0 USD
     '    1 HKD
     '    2 CAD
@@ -12,16 +9,13 @@
     '    7 GBP
     '    8 JPY
     '    9 EUR
-
-
-
-
     Private Sub cbFrom_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbFrom.SelectedIndexChanged
         Dim imageResources() As Image = {My.Resources._0us, My.Resources._1hk, My.Resources._2ca, My.Resources._3in, My.Resources._4lk, My.Resources._5ae, My.Resources._6ph, My.Resources._7gb, My.Resources._8jp, My.Resources._9eu}
 
         If cbFrom.SelectedIndex >= 0 AndAlso cbFrom.SelectedIndex < imageResources.Length Then
             pbFrom.Image = imageResources(cbFrom.SelectedIndex)
         End If
+
 
     End Sub
 
@@ -34,6 +28,17 @@
 
     End Sub
     Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim result, amount As Double
 
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        cbFrom.SelectedIndex = -1
+        cbTo.SelectedIndex = -1
+
+        txtFrom.Text = ""
+        txtTo.Text = ""
     End Sub
 End Class
