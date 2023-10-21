d44cafb4f174b2b2771125e4008855bff1b42988
AllanPZamora <gelgelacc1@gmail.com>
2023-10-22 03:24:49 +0800

    Starting to make menu

---
+++ Zamora, Allan Gel P. (Games)/Games/Games/Main.vb
@@ -0,0 +1,6 @@
+﻿Public Class Main
+
+    Private Sub BackPanel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BackPanel.BackColor = Color.FromArgb(100, 0, 0, 0)
+    End Sub
+End Class
---
+++ Zamora, Allan Gel P. (Games)/Games/Games/Welcome.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Welcome
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(5, Byte), Integer), CType(CType(191, Byte), Integer), CType(CType(219, Byte), Integer))
+        Me.Panel1.Location = New System.Drawing.Point(-26, -4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(602, 51)
+        Me.Panel1.TabIndex = 0
+        '
+        'Welcome
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(8, Byte), Integer), CType(CType(131, Byte), Integer), CType(CType(149, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(556, 313)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Welcome"
+        Me.Text = "Welcome"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+End Class
